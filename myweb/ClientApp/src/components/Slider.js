a00598261a761ea13037c84458a5422785687ee9
YoonkunLee <ydx9474@aut.ac.nz>
2020-02-05 21:34:33 +1300

    Update slider page

---
+++ myweb/ClientApp/src/components/Slider.js
@@ -0,0 +1,25 @@
+﻿import { Component } from 'react'
+
+const ANIM_DURATION = 300
+const FRAME_TIME = 10
+
+export default class Slider extends Component {
+    constructor(props) {
+        super(props)
+        this.mSeriesData = props.seriesData
+        this.mSliderRef = React.createRef();
+        this.state = { isHideLeft: true, isHideRight: false; }
+        this.mScrolled = 0;
+    }
+
+    componentDidMount() {
+        this.refreshButtonHide()
+    }
+
+    getSliderPadding() {
+        return this.mSliderRef.current.firstChild.offsetLeft
+    }
+
+
+
+}
